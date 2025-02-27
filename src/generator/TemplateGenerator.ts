b4eddf01aba8047f61f5d678b453e3916e83b9e2
KristofersOzolinsMagebit <kristofers.ozolins@magebit.com>
2025-02-27 15:28:47 +0200

    fix: invalid module registration

--- src/generator/TemplateGenerator.ts
+++ src/generator/TemplateGenerator.ts
@@ -7,13 +7,17 @@ export default class TemplateGenerator extends FileGenerator {
   public constructor(
     protected fileName: string,
     protected templateName: string,
-    protected data: any
+    protected data: Record<string, any>
   ) {
     super();
   }
 
+  public getTemplateData(): Record<string, any> {
+    return this.data;
+  }
+
   public async generate(workspaceUri: Uri): Promise<GeneratedFile> {
-    const content = await GenerateFromTemplate.generate(this.templateName, this.data);
+    const content = await GenerateFromTemplate.generate(this.templateName, this.getTemplateData());
 
     const path = Uri.joinPath(workspaceUri, this.fileName);
     return new GeneratedFile(path, content);
--- src/generator/module/ModuleRegistrationGenerator.ts
+++ src/generator/module/ModuleRegistrationGenerator.ts
@@ -1,38 +1,30 @@
 import { ModuleWizardComposerData, ModuleWizardData } from 'wizard/ModuleWizard';
-import { PhpFile, PsrPrinter } from 'node-php-generator';
 import { Uri } from 'vscode';
 import GeneratedFile from 'generator/GeneratedFile';
 import Magento from 'util/Magento';
-import FileGenerator from 'generator/FileGenerator';
+import TemplateGenerator from 'generator/TemplateGenerator';
+import FileHeader from 'common/php/FileHeader';
 
-export default class ModuleRegistrationGenerator extends FileGenerator {
+export default class ModuleRegistrationGenerator extends TemplateGenerator {
   public constructor(protected data: ModuleWizardData | ModuleWizardComposerData) {
-    super();
+    super('registration.php', 'php/registration', data);
+  }
+
+  public getTemplateData(): any {
+    const data = super.getTemplateData();
+
+    data.fileHeader = FileHeader.getHeaderAsComment(`${this.data.vendor}_${this.data.module}`);
+
+    return data;
   }
 
   public async generate(workspaceUri: Uri): Promise<GeneratedFile> {
-    const registrationContent = this.getRegistrationContent();
-    const registrationFileUri = Magento.getModuleDirectory(
+    const moduleDirectory = Magento.getModuleDirectory(
       this.data.vendor,
       this.data.module,
-      workspaceUri,
-      'registration.php'
+      workspaceUri
     );
-    return new GeneratedFile(registrationFileUri, registrationContent);
-  }
 
-  protected getRegistrationContent(): string {
-    const printer = new PsrPrinter();
-    const file = new PhpFile();
-    file.setStrictTypes(true);
-    file.addUse('Magento\\Framework\\Component\\ComponentRegistrar');
-
-    const moduleName = Magento.getModuleName(this.data.vendor, this.data.module);
-
-    let content = printer.printFile(file);
-
-    content += `ComponentRegistrar::register(ComponentRegistrar::MODULE, '${moduleName}', __DIR__);\n`;
-
-    return content;
+    return super.generate(moduleDirectory);
   }
 }
