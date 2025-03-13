820daca4c3610964665e5850404ba82131c9f554
raideer <hi@kristofers.dev>
2025-03-13 20:33:58 +0200

    chore: migrate to handlebars

--- src/generator/util/GenerateFromTemplate.ts
+++ src/generator/util/GenerateFromTemplate.ts
@@ -1,23 +1,40 @@
-import { render } from 'ejs';
+import { create } from 'handlebars';
 import { resolve } from 'path';
 import FileSystem from 'util/FileSystem';
-import Logger from 'util/Logger';
 import { Uri } from 'vscode';
+import Logger from 'util/Logger';
 
 export default class GenerateFromTemplate {
-  public static async generate(template: string, data?: any): Promise<string> {
+  public static async generate(template: string, data?: Record<string, any>): Promise<string> {
     try {
       const templatePath = this.getTemplatePath(template);
       const templateContent = await FileSystem.readFile(Uri.file(templatePath));
-      const content = render(templateContent, data);
+      const handlebars = create();
+      this.registerHelpers(handlebars);
+      this.registerPartials(handlebars);
+      const compiledTemplate = handlebars.compile(templateContent);
+      const content = compiledTemplate(data);
       return content;
     } catch (error) {
-      console.error(error);
+      Logger.log('Failed to generate template', String(error));
       throw error;
     }
   }
 
   protected static getTemplatePath(templateName: string): string {
-    return resolve(FileSystem.getExtensionPath('templates'), templateName + '.ejs');
+    return resolve(FileSystem.getExtensionPath('templates/handlebars'), templateName + '.hbs');
+  }
+
+  protected static registerHelpers(handlebars: typeof Handlebars): void {
+    handlebars.registerHelper('ifeq', (a: string, b: string, options: any) => {
+      if (a === b) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    });
+  }
+
+  protected static registerPartials(handlebars: typeof Handlebars): void {
+    handlebars.registerPartial('fileHeader', '{{#if fileHeader}}\n{{{fileHeader}}}\n{{/if}}');
   }
 }
