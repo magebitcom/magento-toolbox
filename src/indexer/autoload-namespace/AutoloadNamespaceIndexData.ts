f685babfdf89d0904b5a0a2d2d14e87ebdabca13
raideer <hi@kristofers.dev>
2025-04-05 23:29:11 +0300

    feat: index all namespaces

--- src/indexer/autoload-namespace/AutoloadNamespaceIndexData.ts
+++ src/indexer/autoload-namespace/AutoloadNamespaceIndexData.ts
@@ -1,78 +1,42 @@
 import { Uri } from 'vscode';
 import PhpNamespace from 'common/PhpNamespace';
-import FileSystem from 'util/FileSystem';
 import { AbstractIndexData } from 'indexer/AbstractIndexData';
-import { AutoloadNamespaceData } from './types';
 import { Memoize } from 'typescript-memoize';
 import AutoloadNamespaceIndexer from './AutoloadNamespaceIndexer';
+import { Namespace } from './types';
 
-export class AutoloadNamespaceIndexData extends AbstractIndexData<AutoloadNamespaceData> {
+export class AutoloadNamespaceIndexData extends AbstractIndexData<Namespace[]> {
   private static readonly SPECIAL_CLASSNAMES = ['Proxy', 'Factory'];
+
+  @Memoize({
+    tags: [AutoloadNamespaceIndexer.KEY],
+  })
+  public getNamespaces(): Namespace[] {
+    return Array.from(this.data.values()).flat();
+  }
 
   @Memoize({
     tags: [AutoloadNamespaceIndexer.KEY],
     hashFunction: (namespace: PhpNamespace) => namespace.toString(),
   })
-  public async findClassByNamespace(namespace: PhpNamespace): Promise<Uri | undefined> {
-    const parts = namespace.getParts();
+  public async findUriByNamespace(phpNamespace: PhpNamespace): Promise<Uri | undefined> {
+    const namespaces = this.getNamespaces();
 
-    for (let i = parts.length; i >= 0; i--) {
-      const namespace = PhpNamespace.fromParts(parts.slice(0, i)).toString();
-
-      const directories = this.getDirectoriesByNamespace(namespace);
-
-      if (directories.length === 0) {
-        continue;
-      }
-
-      let className = parts.pop() as string;
-
-      if (AutoloadNamespaceIndexData.SPECIAL_CLASSNAMES.includes(className)) {
-        className = parts.pop() as string;
-      }
-
-      const classNamespace = PhpNamespace.fromParts(parts.slice(i)).append(className);
-
-      const directory = await this.findNamespaceDirectory(classNamespace, directories);
-
-      if (!directory) {
-        continue;
-      }
-
-      const classPath = classNamespace.toString().replace(/\\/g, '/');
-      const fileUri = Uri.joinPath(directory, `${classPath}.php`);
-
-      return fileUri;
+    if (AutoloadNamespaceIndexData.SPECIAL_CLASSNAMES.includes(phpNamespace.getTail())) {
+      phpNamespace.pop();
     }
 
-    return undefined;
+    const namespace = namespaces.find(n => n.fqn === phpNamespace.toString());
+
+    if (!namespace) {
+      return undefined;
+    }
+
+    return Uri.file(namespace.path);
   }
 
-  private getDirectoriesByNamespace(namespace: string): string[] {
-    const namespaceData = this.getValues().filter(data => data[namespace] !== undefined);
-
-    if (!namespaceData) {
-      return [];
-    }
-
-    return namespaceData.flatMap(data => data[namespace] ?? []);
-  }
-
-  private async findNamespaceDirectory(
-    namespace: PhpNamespace,
-    directories: string[]
-  ): Promise<Uri | undefined> {
-    for (const directory of directories) {
-      const directoryUri = Uri.file(directory);
-      const classPath = namespace.toString().replace(/\\/g, '/');
-      const fileUri = Uri.joinPath(directoryUri, `${classPath}.php`);
-      const exists = await FileSystem.fileExists(fileUri);
-
-      if (exists) {
-        return directoryUri;
-      }
-    }
-
-    return undefined;
+  public findNamespacesByPrefix(prefix: string): Namespace[] {
+    const namespaces = this.getNamespaces();
+    return namespaces.filter(namespace => namespace.fqn.startsWith(prefix));
   }
 }
