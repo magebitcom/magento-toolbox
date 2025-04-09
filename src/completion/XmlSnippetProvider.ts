bec5ff2e1b3c77eb776d1e6bae77195de718531d
raideer <hi@kristofers.dev>
2025-04-09 19:17:52 +0300

    feat: more snippets

--- src/completion/XmlSnippetProvider.ts
+++ src/completion/XmlSnippetProvider.ts
@@ -1,3 +1,4 @@
+import { last, uniq } from 'lodash-es';
 import { minimatch } from 'minimatch';
 import * as vscode from 'vscode';
 
@@ -5,6 +6,7 @@ interface Snippet {
   prefix: string;
   body: string[];
   description: string;
+  parent?: string;
 }
 
 interface SnippetProvider {
@@ -18,7 +20,19 @@ export class XmlSnippetProvider implements vscode.CompletionItemProvider {
   private readonly snippetProviders: SnippetProvider[] = [
     {
       pattern: '**/di.xml',
-      snippets: require('./xml/snippet/di-xml.json'),
+      snippets: require('./xml/snippet/di.json'),
+    },
+    {
+      pattern: '**/events.xml',
+      snippets: require('./xml/snippet/events.json'),
+    },
+    {
+      pattern: '**/crontab.xml',
+      snippets: require('./xml/snippet/crontab.json'),
+    },
+    {
+      pattern: '**/webapi.xml',
+      snippets: require('./xml/snippet/webapi.json'),
     },
   ];
 
@@ -34,10 +48,16 @@ export class XmlSnippetProvider implements vscode.CompletionItemProvider {
       return null;
     }
 
+    const parentElementNames = this.getParentElementNames(document, position);
+    const directParentName = last(parentElementNames);
     const completionItems: vscode.CompletionItem[] = [];
 
     for (const name in snippets) {
       const snippet = snippets[name];
+
+      if (snippet.parent && snippet.parent !== directParentName) {
+        continue;
+      }
 
       const completionItem = new vscode.CompletionItem(
         {
@@ -70,5 +90,45 @@ export class XmlSnippetProvider implements vscode.CompletionItemProvider {
     }
 
     return undefined;
+  }
+
+  private getParentElementNames(
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): string[] {
+    const text = document.getText(new vscode.Range(0, 0, position.line, position.character));
+    // Match all opening elements except for self-closing ones
+    const openingElementMatches = text.matchAll(/<([:_a-zA-Z]+)[^<]*(?<!\/)>/gms);
+    // Match all closing elements
+    const closingElementMatches = text.matchAll(/<\/([:_a-zA-Z]+)>/g);
+
+    if (!openingElementMatches || !closingElementMatches) {
+      return [];
+    }
+
+    const openingElementNames = Array.from(openingElementMatches).map(match => match[1]);
+    const closingElementNames = Array.from(closingElementMatches).map(match => match[1]);
+
+    const openingElementCount = openingElementNames.reduce(
+      (acc, name) => {
+        acc[name] = (acc[name] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+    const closingElementCount = closingElementNames.reduce(
+      (acc, name) => {
+        acc[name] = (acc[name] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+    return uniq(
+      openingElementNames.filter(name => {
+        return openingElementCount[name] > (closingElementCount[name] || 0);
+      })
+    );
   }
 }
